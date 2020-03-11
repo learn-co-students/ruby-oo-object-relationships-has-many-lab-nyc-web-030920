58c7ff3d42ff07a052f149a25e04fa2c3cf7f827
bbedevian <brettbedevian@gmail.com>
2020-03-11 10:58:43 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+attr_accessor :name, :song
+@@song_count = 0 
+    def initialize(name)
+        @name = name
+    end 
+
+    def add_song(song)
+    end 
+
+    def add_song_by_name(name)
+
+    end 
+    
+
+
+
+
+end 
