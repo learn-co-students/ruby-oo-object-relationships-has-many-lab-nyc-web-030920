546fe3fc736df5cb265723537a4c56e438ff948d
bbedevian <brettbedevian@gmail.com>
2020-03-11 11:03:49 -0400

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,9 @@
 class Post
+    @@posts
     attr_accessor :title, :author
     def initialize(title)
         @title = title 
+        @@posts << self
     end 
 
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song
+    @@songs = []
     attr_accessor :name, :artist
     def initialize(name)
         @name = name
+        @@songs << self
     end 
 
 end 
