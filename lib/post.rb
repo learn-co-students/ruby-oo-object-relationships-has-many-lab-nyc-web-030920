00ea21a1d954b8592310898f47c9f111da76a817
toastcorners <wise.melissa19@gmail.com>
2020-03-11 11:03:57 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+    attr_accessor :title
+
+    def initialize(title)
+        @title = title
+    end
+
+
+    def add_post_by_title(title)
+
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+    attr_accessor :name
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+    end
+
+    def add_song_by_name(name)
+        @@all << name
+    end
+
+    def all
+    
+    end
+
+    def 
+
+
+
+
+end
